69647bb31530f4e532f73d1283c92051ab26060c
lifehackerhansol <lifehacker@hansol.ca>
2022-05-02 19:22:24 -0700

    utils: fix traceback

--- utils/utils.py
+++ utils/utils.py
@@ -55,7 +55,7 @@ def command_signature(command, *, prefix=".") -> str:
 
 def create_error_embed(ctx, exc) -> discord.Embed:
     embed = discord.Embed(title=f"Unexpected exception in command {ctx.command}", color=0xe50730)
-    trace = "".join(traceback.format_exception(etype=None, value=exc, tb=exc.__traceback__))
+    trace = "".join(traceback.format_exception(type(exc), value=exc, tb=exc.__traceback__))
     embed.description = f'```py\n{trace}```'
     embed.add_field(name="Exception Type", value=exc.__class__.__name__)
     embed.add_field(name="Information", value=f"channel: {ctx.channel.mention if isinstance(ctx.channel, discord.TextChannel) else 'Direct Message'}\ncommand: {ctx.command}\nmessage: {ctx.message.content}\nauthor: {ctx.author.mention}", inline=False)
