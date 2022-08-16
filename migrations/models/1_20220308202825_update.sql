f3e786a54061229bfa801ac569adc79a74ef713c
lifehackerhansol <lifehacker@hansol.ca>
2022-08-16 16:39:39 -0700

    migrations: nuke it

--- migrations/models/1_20220308202825_update.sql
+++
@@ -1,7 +0,0 @@
--- upgrade --
-CREATE TABLE IF NOT EXISTS "modrole" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "guild_id" BIGINT NOT NULL REFERENCES "guild" ("id") ON DELETE CASCADE
-);
--- downgrade --
-DROP TABLE IF EXISTS "modrole";
--- migrations/models/4_20220308224417_update.sql
+++
@@ -1,4 +0,0 @@
--- upgrade --
-ALTER TABLE "guild" ADD "autorole" INT NOT NULL  DEFAULT 0;
--- downgrade --
-ALTER TABLE "guild" DROP COLUMN "autorole";
