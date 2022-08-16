9a494bbfd2db27f76c8b16681fc7f452702d2418
lifehackerhansol <lifehacker@hansol.ca>
2022-08-16 16:39:39 -0700

    migrations: nuke it

--- migrations/models/3_20220308222645_update.sql
+++
@@ -1,6 +0,0 @@
--- upgrade --
-ALTER TABLE "guild" ADD "mb2" INT NOT NULL  DEFAULT 0;
-ALTER TABLE "guild" ADD "xyd" INT NOT NULL  DEFAULT 0;
--- downgrade --
-ALTER TABLE "guild" DROP COLUMN "mb2";
-ALTER TABLE "guild" DROP COLUMN "xyd";
